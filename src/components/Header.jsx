a1db4508b6d287bd49043a8e2ae75edfcece2c0f
JayveeErandio <jayveeerandio@gmail.com>
2026-03-31 19:57:19 +0800

    Scaling in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div className="flex w-full items-center gap-3 justify-between">
+    <div className="flex w-full items-center gap-4 justify-between">
       <SearchBar className="flex-1" onSearch={setSearchTerm} />
       <img src="icons/darkmode.svg" className="h-7" />
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,13 +10,13 @@ export default function SearchBar({ onSearch }) {
 
   return (
     <div className="shadow-md bg-white flex items-center rounded-4xl px-3 w-full">
-      <img src="icons/search.svg" className="opacity-75 h-5.5 " />
+      <img src="icons/search.svg" className="opacity-75 h-6 " />
       <input
         type="text"
         value={query}
         onChange={handleChange}
         placeholder="Search for chips, soap bar, etc..."
-        className="outline-none text-xs text-gray-700 px-2 py-3 w-full"
+        className="outline-none text-sm text-gray-700 px-2 py-3 w-full"
       />
     </div>
   );
